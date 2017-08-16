61b7628a8b014970d89e5df4e86b233ba04fca72
Carlos Sales <carlossalesti@gmail.com>
2017-08-16 19:00:08 -0300

    Cad novo usuario - em andamento

---
+++ MeuPelo/App_Code/WebService.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Web.Services;
+using System.Data.SqlClient;
+
+[WebService(Namespace = "http://meupelo.azurewebsites.net/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string usersave(string param1, string param2, string param3, string param4, string param5,
+         string param6, string param7)
+    {
+
+        string url = "Sorry.html";
+
+        OperacaoBanco operacao = new OperacaoBanco();
+        bool inserir = operacao.Insert("INSERT INTO Tbl_Usuarios (nome ,usuario,senha,endereco,numero,complemento,telefone,dataCadastro) " +
+            "VALUES ('" + param1 + "', '" + param2 + "', '" + param3 + "', '" + param4 + "', '" + param5 +
+            "', '" + param6 + "', '" + param7 + "', dateadd(hh,-3,getdate()) )");
+        ConexaoBancoSQL.fecharConexao();
+
+        if (inserir == true)
+        {
+            string stringselect = "select ID_User,nome,usuario " +
+                "from Tbl_Usuarios " +
+                "where usuario='" + param2 + "' and senha='" + param3 + "' ";
+
+            OperacaoBanco operacao1 = new OperacaoBanco();
+            SqlDataReader dados = operacao1.Select(stringselect);
+
+            while (dados.Read())
+            {
+                //cria parametros para validação de acesso
+                string vValida1 = DateTime.Now.ToString("hh"); // hora
+                string vValida2 = DateTime.Now.ToString("mm"); // minuto
+                int vValida3 = Convert.ToInt16(vValida1) * Convert.ToInt16(vValida2);
+                string vValida4 = vValida3.ToString();
+
+                url = "Redirect.aspx?v1=" + Convert.ToString(dados[0]) + "&v2=" + Convert.ToString(dados[1]) +
+                    "&v3=" + vValida4 + "&v4=0" + "&v5=NewUser";
+            }
+
+        }
+
+        ConexaoBancoSQL.fecharConexao();
+        return url;
+    }
+
+}
+
+
+public class ConexaoBancoSQL
+{
+    private static SqlConnection objConexao = null;
+    private string stringconnection1;
+
+    public void tentarAbrirConexaoRemota()
+    {
+        objConexao = new SqlConnection();
+        objConexao.ConnectionString = stringconnection1;
+        objConexao.Open();
+    }
+
+    public ConexaoBancoSQL()
+    {
+        // *** STRING DE CONEXÃO COM BANCO DE DADOS - Atenção! Alterar dados conforme seu servidor
+        stringconnection1 = "Server=tcp:serverlogvai.database.windows.net,1433;Initial Catalog=dblogvai;Persist Security Info=False;User ID=admserver;Password=pwd@2017;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        try
+        {
+            tentarAbrirConexaoRemota();
+        }
+        catch
+        {
+            Console.WriteLine("Atenção! Não foi possível Conectar ao Servidor de Banco de Dados.");
+        }
+    }
+
+    public static SqlConnection getConexao()
+    {
+        new ConexaoBancoSQL();
+        return objConexao;
+    }
+    public static void fecharConexao()
+    {
+        objConexao.Close();
+    }
+}
+
+public class OperacaoBanco
+{
+    private SqlCommand TemplateMethod(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return Commando;
+        }
+        catch
+        {
+            return Commando;
+        }
+    }
+
+    public SqlDataReader Select(String query)
+    {
+        SqlDataReader dadosObtidos = TemplateMethod(query).ExecuteReader();
+        return dadosObtidos;
+    }
+
+    public Boolean Insert(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public Boolean Update(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public Boolean Delete(String query)
+    {
+        SqlConnection Conexao = ConexaoBancoSQL.getConexao();
+        SqlCommand Commando = new SqlCommand(query, Conexao);
+        try
+        {
+            Commando.ExecuteNonQuery();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+}
--- MeuPelo/scripts/codeSignIn.js
+++ MeuPelo/scripts/codeSignIn.js
@@ -1,3 +1,44 @@
 ﻿function NovoUsuario() {
 
+    var v1 = document.getElementById("input_nomeRazao").value;
+    var v2 = document.getElementById("input_User").value;
+
+    var v3 = document.getElementById("input_psw").value;
+    var v3a = document.getElementById("input_pswConf").value;
+    
+    var v4 = document.getElementById("input_endereco").value;
+    var v5 = document.getElementById("input_numero").value;
+    var v6 = document.getElementById("input_complemento").value;
+    var v7 = document.getElementById("input_telefone").value;
+
+    if (v1 == "") { alert('Informe Nome ou Razão Social!'); document.getElementById('input_nomeRazao').focus(); return; }
+
+    if (v2 == "") { alert('Informe e-mail!'); document.getElementById('input_User').focus(); return; }
+    if (v3 == "") { alert('Informe Senha!'); document.getElementById('input_psw').focus(); return; }
+    if (v3 != v3a) { alert('Senhas não conferem!'); document.getElementById('input_pswConf').focus(); return; }
+
+    if (v4 == "") { alert('Informe endereço!'); document.getElementById('input_endereco').focus(); return; }
+    if (v5 == "") { alert('Informe número do endereço!'); document.getElementById('input_numero').focus(); return; }
+    if (v7 == "") { alert('Informe telefone!'); document.getElementById('input_telefone').focus(); return; }
+
+    document.getElementById("btSignIn").style.cursor = "progress";
+    document.getElementById("btSignIn").disabled = true;
+    document.getElementById('divHidden').style.display = 'block';
+
+    $.ajax({
+        type: "POST",
+        url: "WebService.asmx/usersave",
+        data: '{param1: "' + v1 + '", param2: "' + v2 + '", param3: "' + v3 + '", param4: "' + v4 + '", param5: "' + v5 +
+            '", param6: "' + v6 + '", param7: "' + v7 + '"}',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            var linkurl = response.d;
+            window.open(linkurl, '_parent');
+        },
+        failure: function (response) {
+            document.getElementById("btSignIn").style.cursor = "pointer";
+            alert('Tente Novamente');
+        }
+    });
 }
